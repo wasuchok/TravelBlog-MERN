42ccd6d6230d03f3b5e23815b1580abeb928ee2f
Wasuchok Jainam <wasuchokop@gmail.com>
2023-03-04 17:06:45 +0700

    new file:   .env 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	modified:   src/main.jsx 	new file:   src/redux/actions/blogPostActions.js 	new file:   src/redux/slices/blogPost.js 	new file:   src/redux/store.js 	new file:   src/screens/BlogScreen.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import BlogScreen from './screens/BlogScreen';
 
 
 const App = () => {
@@ -9,7 +10,9 @@ const App = () => {
       <Router>
       <Navbar />
       <main>
-        <Routes></Routes>
+      <Routes>
+            <Route path='/blog/:category' element={<BlogScreen />}></Route>
+          </Routes>
       </main>
       </Router>
     </ChakraProvider>
